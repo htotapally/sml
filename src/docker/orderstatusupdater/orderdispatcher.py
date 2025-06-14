2eb5830a9a316e9d44418e1a8b7cfab44c1a12dc
htotapally <htotapally@yahoo.com>
2025-06-14 06:50:46 -0400

    Add files via upload

---
+++ src/docker/orderstatusupdater/orderdispatcher.py
@@ -0,0 +1,79 @@
+import os.path
+import configparser
+import json
+import requests
+
+from kafka import KafkaProducer
+
+class OrderDispatcher():
+    # Kafka Configuration
+    bootstrap_servers = ['localhost:9092']  # Replace with your Kafka broker address
+    topic_name = 'TutorialTopic'
+    ordersurl = 'http://localhost/os/getorders'
+    ready = True    
+    
+    def __init__(self, dispconf):
+      super().__init__()
+      print (dispconf)
+      config = configparser.ConfigParser()
+      config.read(dispconf)
+      topic = config.get('kafka', 'topic')
+      print(topic)
+      bootstrap = config.get('kafka', 'bootstrap')
+      print (bootstrap)
+      
+      # Set Kafka configuration
+      self.topic_name = topic
+      self.bootstrap_servers = eval(bootstrap)
+      
+      # Url to fetch unfilled orders
+      self.ordersurl = config.get('orders', 'ordersurl')
+
+    def dispatchorders(self):
+      orders = self.getorders()
+      for order in orders:
+          if self.ready:
+            self.dispatchorder(order)
+
+    def dispatchorder(self, cart):
+
+        print(cart)
+        # Create a Kafka producer instance
+        producer = KafkaProducer(
+            bootstrap_servers = self.bootstrap_servers,
+            value_serializer=lambda x: json.dumps(x).encode('utf-8')
+        )
+
+        print("Executing dispatch")
+        # Send message to Kafka
+        try:
+            print("Sending message!")
+            producer.send(self.topic_name, value=cart)
+            producer.flush() # Ensure message is sent
+            print("Message sent successfully!")
+            self.ready = False
+        except Exception as e:
+            print(f"Error sending message: {e}")
+        finally:
+            producer.close()        
+
+    def getorders(self):
+      ordersurl = self.ordersurl
+      resp = requests.get(ordersurl)
+      ordereditems = json.loads(resp.text)
+      return ordereditems
+            
+def main(args=None):
+    print("Starting order dispatcher!")
+    dispconf = os.path.join(os.path.dirname(__file__), 'dispconf.conf')
+    try:
+      print('Within the try block')  
+      orderDispatcher = OrderDispatcher(dispconf)
+      orderDispatcher.dispatchorders()
+      print('After dispatchorders')
+    except (KeyboardInterrupt):
+        pass
+
+
+if __name__ == '__main__':
+    main()    
