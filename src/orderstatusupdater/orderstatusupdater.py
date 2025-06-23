54d6ec96ac814384e6b446663ab84d84b42686b7
htotapally <htotapally@yahoo.com>
2025-06-23 11:08:18 -0400

    Add files via upload

--- src/orderstatusupdater/orderstatusupdater.py
+++ src/orderstatusupdater/orderstatusupdater.py
@@ -6,6 +6,13 @@ import threading
 
 from kafka import KafkaConsumer
 
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+
 class OrderStatusUpdater():
     def __init__(self):
       super().__init__()
@@ -13,6 +20,13 @@ class OrderStatusUpdater():
 def process_message(message, orderupdateurl):
     # Custom message processing logic
     print(f"Processing message: {message.value}")
+    with tracer.start_as_current_span("UpdateSpan"):
+        with tracer.start_as_current_span("childSpan") as parent_span:
+            parent_span.add_event("Dispatching order.", {
+                "message_type": "info",
+                    "message": message.value
+                }) 
+
     data = json.loads(message.value)
     orderid = data['orderid']
     print(orderid)
@@ -64,6 +78,34 @@ def main(args=None):
 
     for thread in threads:
         thread.join()
-    
+
+provider = TracerProvider(
+  resource = Resource.create({SERVICE_NAME: "OrderStatusUpdaterService"})
+)
+
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
+jaeger_exporter = OTLPSpanExporter(                                                 
+  endpoint = "http://192.168.1.170:4318/v1/traces"                                  
+)                                                                                   
+                                                                                    
+if os.path.join(os.path.dirname(__file__), '/config/dispconf.conf'):                
+    dispconf = os.path.join(os.path.dirname(__file__), '/config/dispconf.conf')     
+    print("Recreating the exporter")                                                
+    config = configparser.ConfigParser()                                            
+    config.read(dispconf)                                                           
+    oltpExporterEndpoint = config.get('OTLPSpanExporter', 'oltpExporterEndpoint')   
+    print(oltpExporterEndpoint)                                                     
+    jaeger_exporter = OTLPSpanExporter(                                             
+        endpoint = oltpExporterEndpoint                                             
+    ) 
+
+trace.get_tracer_provider().add_span_processor(
+   BatchSpanProcessor(jaeger_exporter)
+)    
+
 if __name__ == '__main__':
     main()    
