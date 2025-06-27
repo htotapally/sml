d9bac294f142f8b70efb38c3fa2d4b3428596a5c
htotapally <htotapally@yahoo.com>
2025-06-27 15:17:51 -0400

    Update init.sql

--- src/postgres/init.sql
+++ src/postgres/init.sql
@@ -5,9 +5,6 @@ CREATE TABLE onlineorders (
     id SERIAL PRIMARY KEY,
     createtime TIMESTAMP NOT NULL DEFAULT NOW(),
     OrderId uuid,
-    ItemId TEXT NOT NULL,
-    Qty INT NOT NULL,
-    SalePrice NUMERIC(6, 2),
     Status TEXT NOT NULL DEFAULT 'Created'
 );
 
