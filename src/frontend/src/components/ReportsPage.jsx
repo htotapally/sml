261698adc85c1018f0f5e80d757d71e989eca79a
htotapally <htotapally@yahoo.com>
2025-07-21 16:38:41 -0400

    Add files via upload

---
+++ src/frontend/src/components/ReportsPage.jsx
@@ -0,0 +1,67 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { embedDashboard } from "@superset-ui/embedded-sdk";
+
+import { ThemeContext } from './ThemeContext'
+
+function ReportsPage({ setCurrentPage }) {
+  const BackendURL = 'http://192.168.1.153:3001'
+  const getToken = async () => {
+    console.log('Getting Token')
+    const dashboardId = "00b809e7-324b-4204-affb-abc41af6e71c"
+    const params = {
+      dashboardId: dashboardId,
+    }
+
+    const url = new URL(BackendURL + '/guest-token')
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+
+    const response = await fetch(url)
+    const data = await response.json();
+    const token = data.token;
+    console.log("Received guest token:", token);
+    return token;
+  };
+
+  const dashboardId = "00b809e7-324b-4204-affb-abc41af6e71c" // "1bcb91ee-3441-4e56-8900-d23e2498f888"
+  const supersetDomain = "http://192.168.1.170:8088"
+  useEffect(() => {
+
+    console.log("Reports page has been loaded")
+
+    if (document.getElementById("dashboard")) {
+      // const embeddedFrame = 
+      const element = document.getElementById("dashboard")
+      console.log(element.children[0])
+      element.style.width = "100%";
+      element.style.height = "100%";
+    }
+
+    const embed = async () => {
+      await embedDashboard({
+        id: dashboardId,
+        supersetDomain: supersetDomain,
+        mountPoint: document.getElementById("dashboard"),
+        fetchGuestToken: (dashboardId) => getToken(dashboardId),
+        dashboardUiConfig: {
+          hideTitle: true,
+          hideChartControls: true,
+          hideTab: true,
+        },
+      });
+    };
+
+    if (document.getElementById("dashboard")) {
+      embed();
+      let iframeElement = document.querySelector('iframe');
+      iframeElement.style.height = '900px'
+      iframeElement.style.width = '1600px'
+      console.log(iframeElement)
+    }
+  }, []);
+
+  return (
+       <div id="dashboard"></div>
+  );
+}
+
+export default ReportsPage;
